f1c34a88bec7863b004c6e7fc99fd06b9b217fc5
tmy628 <tmy6h1r@gmail.com>
2021-04-11 00:46:14 +0000

    [Setting/Finish] 「マウスクリック」イベントを設定する/終了

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,5 +1,5 @@
 $(function(){
-  $('.box1').mouseover(function(){
+  $('.box1').on('click', function(){
     $('.box1').addClass('box1-ext');
   });
   $('.box1').mouseout(function(){
@@ -7,13 +7,22 @@ $(function(){
   });
 });
 
+
+// $('.セレクタ名').on('click', function() {
+//   イベント発生時に行われる処理
+// });
 // $('.セレクタ名').イベント名(function(){
 //   イベント発生時に行われる処理
 // });
 
-// .box1-extが、マウスオーバー時に適用されるCSS（class）
-// マウスオーバーすると.box1-extのclassが追加され、マウスアウトすると.box1-extのclassが外れる
+// HTML要素がクリックされたときに、以下の2つのイベントが行われるように設定する
+// .box1がクリックされたときは、box1-extのclassを追加する
+// マウスアウトされたときは、box1-extのclassを削除する
 
 // class属性の切り替えに使われているのが、addClass( )とremoveClass( )
 // addClass( )では、対象のHTML要素にclass属性を追加する
 // removeClass( )では、要素に設定されているclass属性が解除される
+
+// マウスクリックのイベントには、.click( )もある
+// clickの場合は、クリックされた時点でイベントが終わってしまう
+// 今回は、クリック後に複数のイベントが行われるようにしたいので、onを付けた書き方をする
