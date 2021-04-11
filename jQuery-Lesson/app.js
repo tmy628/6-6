0a744346466e18a85900d8c0e743e5f324b620f5
tmy628 <tmy6h1r@gmail.com>
2021-04-11 00:39:54 +0000

    [Add] class属性を追加する／解除する

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,12 +1,19 @@
 $(function(){
   $('.box1').mouseover(function(){
-    $('.box1').css({'background-color': '#0000FF'});
+    $('.box1').addClass('box1-ext');
   });
   $('.box1').mouseout(function(){
-    $('.box1').css({'background-color': '#FF0000'});
+    $('.box1').removeClass('box1-ext');
   });
 });
 
 // $('.セレクタ名').イベント名(function(){
 //   イベント発生時に行われる処理
 // });
+
+// .box1-extが、マウスオーバー時に適用されるCSS（class）
+// マウスオーバーすると.box1-extのclassが追加され、マウスアウトすると.box1-extのclassが外れる
+
+// class属性の切り替えに使われているのが、addClass( )とremoveClass( )
+// addClass( )では、対象のHTML要素にclass属性を追加する
+// removeClass( )では、要素に設定されているclass属性が解除される
