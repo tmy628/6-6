3fe8b402d658d2c88e8610a95abbe1374ab2d47e
tmy628 <tmy6h1r@gmail.com>
2021-04-11 00:36:16 +0000

    [Add] 「マウスアウト」イベントを追加する

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -2,6 +2,9 @@ $(function(){
   $('.box1').mouseover(function(){
     $('.box1').css({'background-color': '#0000FF'});
   });
+  $('.box1').mouseout(function(){
+    $('.box1').css({'background-color': '#FF0000'});
+  });
 });
 
 // $('.セレクタ名').イベント名(function(){
